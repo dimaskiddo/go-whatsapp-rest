b65b51d2eac18b014c753551e0356ce9727614d3
Andrei Rosseti <andrei@facedigital.com.br>
2020-02-29 17:12:06 -0300

    Just moved the restore routine into main file to work better.

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"path"
+	"path/filepath"
+	"strings"
 	"syscall"
 
 	"github.com/dimaskiddo/go-whatsapp-rest/hlp"
+	"github.com/dimaskiddo/go-whatsapp-rest/hlp/libs"
 	"github.com/dimaskiddo/go-whatsapp-rest/hlp/router"
 )
 
@@ -17,6 +21,7 @@ var svr *hlp.Server
 func init() {
 	// Initialize Server
 	svr = hlp.NewServer(router.Router)
+	restore()
 }
 
 // Main Function
@@ -40,4 +45,31 @@ func main() {
 
 	// Stopping Server
 	defer svr.Stop()
+}
+
+func restore() {
+	files, err := filepath.Glob(hlp.Config.GetString("SERVER_STORE_PATH") + "/*.gob")
+	if err == nil {
+		for _, file := range files {
+			jid := strings.TrimSuffix(filepath.Base(file), path.Ext(file))
+			qrstr := make(chan string)
+			errmsg := make(chan error)
+
+			hlp.LogPrintln(hlp.LogLevelInfo, "whatsapp", "Attempting to restore session state for: "+jid)
+
+			go func() {
+				libs.WASessionConnect(jid, 20, file, qrstr, errmsg)
+			}()
+
+			select {
+			case err := <-errmsg:
+				if len(err.Error()) != 0 {
+					hlp.LogPrintln(hlp.LogLevelWarn, "whatsapp", "Failed to restore session: "+err.Error())
+					return
+				}
+
+				hlp.LogPrintln(hlp.LogLevelInfo, "whatsapp", "Session restored: "+jid)
+			}
+		}
+	}
 }
